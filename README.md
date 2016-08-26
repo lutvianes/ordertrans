594ca7e7c37d330b4d1dda9152b3b2883580977e
lutvianes <lutvianes@gmail.com>
2016-08-26 17:01:50 +0700

    Update/Delete Customer

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Order Transaction
-This project implement services for order transaction. Build using [Express.js](https://expressjs.com/) and [Sequelize](sequelizejs.com).
+This project implement services for order transaction. Build using [Express.js](https://expressjs.com/) and [Sequelize](http://sequelizejs.com).
 
-The website is available in [OrderTrans](ordertrans.vians.cf) which is hosted in [Digital Ocean](https://www.digitalocean.com) VPS instance.
+The website is available in [OrderTrans](http://ordertrans.vians.cf) which is hosted in [Digital Ocean](https://www.digitalocean.com) VPS instance.
--- api/controllers/customers.js
+++ api/controllers/customers.js
@@ -5,24 +5,53 @@ function get(req, res, next) {
     Customer.findAll().then(function(customer) {
         return res.json(customer)
     }).catch(function(err) {
-        return next({status: 500, message: err.message})
+        return next(err)
     })
 }
 
 function create(req, res, next) {
-    Customer.create(req.body).then(function(cust) {
-        return res.status(201).location('/customers/'+cust.id).send()
+    Customer.create(req.body).then(function(customer) {
+        return res.status(201).location('/customers/'+customer.id).send()
     }).catch(function(err){
-        return next({status: 500, message: err.message})
+        return next(err)
     })
 }
 
 function update(req, res, next) {
+    if(!req.params.id)
+        return next({status: 404, message: 'Not Found'})
 
+    Customer.update(req.body, {
+        where: {
+            id: req.params.id
+        },
+        limit: 1
+    }).then(function(result) {
+        if (result[0]) // if number of affected rows not 0
+            return res.status(204).send()
+        else
+            return next({status: 404, message: 'Not Found'})
+    }).catch(function(err) {
+        return next(err)
+    })
 }
 
 function remove(req, res, next) {
-    
+    if(!req.params.id)
+        return next({status: 404, message: 'Not Found'})
+
+    Customer.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(function(result) {
+        if (result[0]) // if number of affected rows not 0
+            return res.status(204).send()
+        else
+            return next({status: 404, message: 'Not Found'})
+    }).catch(function(err) {
+        return next(err)
+    })
 }
 
 module.exports = {
