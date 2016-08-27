aef189b900d304479c98a7f317fc73b6a57ce73d
lutvianes <lutvianes@gmail.com>
2016-08-27 16:18:45 +0700

    Add models

--- api/models/product.js
+++ api/models/product.js
@@ -1,4 +1,5 @@
 'use strict';
+var OrderProduct = require('./orderproduct')
 module.exports = function(sequelize, DataTypes) {
   var Product = sequelize.define('Product', {
     name: DataTypes.STRING, // used by actors to search the product
