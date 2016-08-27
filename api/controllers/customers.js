0bcf2b1b306a23dd27c8e785290a825202b7deee
lutvianes <lutvianes@gmail.com>
2016-08-27 10:41:27 +0700

    Separate customer controller from router

--- api/controllers/customers.js
+++ api/controllers/customers.js
@@ -1,58 +1,24 @@
 const Customer = require('../models').Customer
 
 // Get all function
-function get(req, res, next) {
-    Customer.findAll().then(function(results) {
-        return res.json(results)
-    }).catch(function(err) {
-        return next(err)
-    })
+function get() {
+    return Customer.findAll()
 }
 
-function create(req, res, next) {
-    Customer.create(req.body).then(function(result) {
-        return res.status(201)
-            .location('http://'+req.hostname+req.baseUrl+'/'+result.id)
-            .send()
-    }).catch(function(err){
-        return next(err)
-    })
+function create(customer) {
+    return Customer.create(customer)
 }
 
-function update(req, res, next) {
-    if(!req.params.id)
-        return next({status: 404, message: 'Not Found'})
-
-    Customer.update(req.body, {
-        where: {
-            id: req.params.id
-        },
+function update(customer, id) {
+    return Customer.update(customer, {
+        where: { id: id },
         limit: 1
-    }).then(function(result) {
-        if (result[0]) // if number of affected rows not 0
-            return res.status(204).send()
-        else
-            return next({status: 404, message: 'Not Found'})
-    }).catch(function(err) {
-        return next(err)
     })
 }
 
-function remove(req, res, next) {
-    if(!req.params.id)
-        return next({status: 404, message: 'Not Found'})
-
-    Customer.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then(function(result) {
-        if (result[0]) // if number of affected rows not 0
-            return res.status(204).send()
-        else
-            return next({status: 404, message: 'Not Found'})
-    }).catch(function(err) {
-        return next(err)
+function remove(id) {
+    return Customer.destroy({
+        where: { id: id }
     })
 }
 
--- api/routes/customers.js
+++ api/routes/customers.js
@@ -5,11 +5,53 @@ const router = express.Router()
 const controller = require('../controllers/customers')
 
 router.route('/')
-    .get(controller.get)
-    .post(controller.create)
+    .get(function(req, res, next) {
+        controller.get()
+            .then(function(results) {
+                return res.json(results)
+            }).catch(function(err) {
+                return next(err)
+            })
+    })
+    .post(function(req, res, next) {
+        controller.create(req.body)
+            .then(function(result) {
+                return res.status(201)
+                    .location('http://'+req.hostname+req.baseUrl+'/'+result.id)
+                    .send()
+            }).catch(function(err) {
+                return next(err)
+            })
+    })
 
 router.route('/:id')
-    .put(controller.update)
-    .delete(controller.remove)
+    .put(function(req, res, next) {
+        if(!req.params.id)
+            return next()
+
+        controller.update(req.body, req.params.id)
+            .then(function(result) {
+                if (result[0]) // if number of affected rows not 0
+                    return res.status(204).send()
+                else
+                    return next()
+            }).catch(function(err) {
+                return next(err)
+            })
+    })
+    .delete(function(req, res, next) {
+        if(!req.params.id)
+            return next()
+
+        controller.remove(req.params.id)
+            .then(function(result) {
+                if (result[0]) // if number of affected rows not 0
+                    return res.status(204).send()
+                else
+                    return next({status: 404, message: 'Not Found'})
+            }).catch(function(err) {
+                return next(err)
+            })
+    })
 
 module.exports = router
